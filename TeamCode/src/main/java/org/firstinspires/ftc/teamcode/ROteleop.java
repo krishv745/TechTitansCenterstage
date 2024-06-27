89f0327df22ce211df8049f3790ac74e9d10401a
krishvadivel <125418482+krishv745@users.noreply.github.com>
2024-06-27 15:43:22 -0500

    Create ROteleop.java
    Allows for movement

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ROteleop.java
@@ -0,0 +1,55 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+@TeleOp
+public class ROteleop extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        // Declare our motors
+        // Make sure your ID's match your configuration
+        DcMotor fl = hardwareMap.dcMotor.get("fl"); //port 0 CH
+        DcMotor bl = hardwareMap.dcMotor.get("bl"); //port 1 CH
+        DcMotor br = hardwareMap.dcMotor.get("br"); //port 2 CH
+        DcMotor fr = hardwareMap.dcMotor.get("fr"); //port 3 CH
+
+        fl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        bl.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        br.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        fr.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        // Reverse the right side motors. This may be wrong for your setup.
+        // If your robot moves backwards when commanded to go forwards,
+        // reverse the left side instead.
+        // See the note about this earlier on this page.
+        fr.setDirection(DcMotorSimple.Direction.REVERSE);
+        br.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            double y = -gamepad1.left_stick_y - gamepad1.right_stick_y; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x * 1.1; // Counteract imperfect strafing
+            double rx = gamepad1.right_stick_x;
+
+            // Denominator is the largest motor power (absolute value) or 1
+            // This ensures all the powers maintain the same ratio,
+            // but only if at least one is out of the range [-1, 1]
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+            double flPower = (y + x + rx) / denominator;
+            double blPower = (y - x + rx) / denominator;
+            double brPower = (y + x - rx) / denominator;
+            double frPower = (y - x - rx) / denominator;
+
+            fl.setPower(flPower);
+            bl.setPower(blPower);
+            br.setPower(brPower);
+            fr.setPower(frPower);
+        }
+    }
+}
