7ca7ce413b16f66af8418509d78c8ba5895d8e71
krishvadivel <125418482+krishv745@users.noreply.github.com>
2024-07-02 01:43:27 -0500

    Update slidesTest.java
    adjusting the slides test to using the dpad (we did this just to check but have to adjust it back to the left or right stick on the controller once servo code is figured out)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/slidesTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/slidesTest.java
@@ -10,31 +10,29 @@ import java.util.concurrent.TimeUnit;
 @TeleOp(group = "ZTest")
 public class slidesTest extends LinearOpMode{
     public void runOpMode() throws InterruptedException {
-        Servo slides1 = hardwareMap.servo.get("slides1");
-        Servo slides2 = hardwareMap.servo.get("slides2");
-        double ljspos = gamepad1.left_stick_y;
+        Servo ls1 = hardwareMap.servo.get("ls1");
+        Servo ls2 = hardwareMap.servo.get("ls2");
+        double ls1Pos  = 0;
+        double ls2Pos  = 0;
+//        ljspos = gamepad1.left_stick_y;
         waitForStart();
         if (isStopRequested()) return;
         while (opModeIsActive()) {
-            if (ljspos > 0.5) {
-                slides1.setPosition(135);
+            if (gamepad1.dpad_up) {
+                ls1Pos += 0.1;
+                ls2Pos += 0.1;
+                ls1.setPosition(ls1Pos);
+                ls2.setPosition(ls2Pos);
                 TimeUnit.MILLISECONDS.sleep(150);
-                if (ljspos == 1.0) {
-                    slides2.setPosition(135);
-                    TimeUnit.MILLISECONDS.sleep(150);
-                }
-            }
-            else if (ljspos < 0.5) {
-                slides1.setPosition(0);
+            } else if (gamepad1.dpad_down) {
+                ls1Pos -= 0.1;
+                ls2Pos -= 0.1;
+                ls1.setPosition(ls1Pos);
+                ls2.setPosition(ls2Pos);
                 TimeUnit.MILLISECONDS.sleep(150);
-                if (ljspos == 0.0) {
-                    slides2.setPosition(0);
-                    TimeUnit.MILLISECONDS.sleep(150);
-                }
             }
-            telemetry.addData("slide1pos: ", slides1.getPosition());
-            telemetry.addData("slide2pos: ", slides2.getPosition());
-            telemetry.addData("jspos: ", ljspos);
+            telemetry.addData("slide1pos: ", ls1.getPosition());
+            telemetry.addData("slide2pos: ", ls2.getPosition());
             telemetry.update();
 
         }
