50a801d0287709da91049260c2c26c8346089f0f
midK-night <pasum462@gmail.com>
2024-07-29 18:41:56 -0500

    Create servoTest class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/servoTest.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Servo;
+
+@TeleOp(group = "ZTest")
+public class servoTest extends LinearOpMode {
+    public void runOpMode() throws InterruptedException {
+        
+    }
+}
