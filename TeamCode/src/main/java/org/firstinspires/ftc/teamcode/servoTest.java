fecb50f00330b33d2e85f334942fb9bbd228f214
midK-night <pasum462@gmail.com>
2024-07-29 19:01:54 -0500

    update button order

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/servoTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/servoTest.java
@@ -21,8 +21,12 @@ public class servoTest extends LinearOpMode {
         while (opModeIsActive) {
             if (gamepad1.dpad_up && pos < 1.0) {
                 pos += 0.05;
+            } else if (gamepad1.dpad_right && pos < 1.0) {
+                post += 0.01;
             } else if (gamepad1.dpad_down && pos > 0.0) {
                 pos -= 0.05;
+            } else if (gamepad1.dpad_left && pos > 0.0) {
+                pos -= 0.01;
             }
 
             servoTest.setPosition(pos);
